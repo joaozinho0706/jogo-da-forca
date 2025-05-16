8759f360440443d28ba791970cea4bd0a8267905
joaozinho0706 <126591273+joaozinho0706@users.noreply.github.com>
2025-05-16 10:59:07 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,24 +3,37 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "Framboesa"
+    palavra_secreta = "Framboesa".upper()
     letras_acertadas = ["_","_","_", "_","_","_","_","_","_"]
 
     enforcou = False
     acertou =  False
-    
+    erros = 0
+
     while (not enforcou and not acertou):
+
         chute = input("Qual é a letra?")
         chute = chute.strip()
         
-        posicao = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-            posicao = posicao + 1
-        
-        
+
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[posicao] = letra
+   
+                posicao = posicao + 1
+
+        else:   
+            erros = erros + 1
+        enforcou = erros == 10
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+        if(acertou):
+            print("voce ganhou!!")
+        else:
+            print("voce perdeu, seu lixo!")
     
     
 
